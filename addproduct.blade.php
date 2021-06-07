886fb90086b3dae5ab6c6894b7b1ca06aa66187c
Fr1on <77189617+Fr1on@users.noreply.github.com>
2021-06-07 17:51:47 +0700

    Add files via upload

---
+++ addproduct.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+@section('main')
+    
+    <div class="backaddprod">
+        <div class="addproducts">
+            <form action="addProductData" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="addproduct">
+                    <h2>Создание товара</h2>
+                    <h5>Название товара</h5>
+                    <input type="text" name="name" placeholder="name" class="addprod">
+                    <h5>Цена товара</h5>
+                    <input type="number" name="price" placeholder="price" class="addprod">
+                    <h5>Категория товара</h5>
+                    <input type="text" name="category" placeholder="category" class="addprod">
+                    <h5>Описание товара</h5>
+                    <textarea name="description" placeholder="description" class="addprod"></textarea>
+                    <input type="file" name="photo" placeholder="photo" class="addprodimg">
+                    <button type="submit" class="btn btn-primary btn-lg">Add Product</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@endsection
+@section('down')
+
+
+@endsection
